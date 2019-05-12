cfe316787d48beb2a315a91762f2bc13ad256f15
r <kubr2017@gmail.com>
2019-05-11 22:23:59 -0400

    Feat: Create Sample of <PostForm> and <AllPost>.

---
+++ src/components/AllPost.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AllPost extends Component{
+  render(){
+    return(
+      <div>
+        <h3>AllPost</h3>
+      </div>
+    )
+  }
+}
+
+export default AllPost;
---
+++ src/components/PostForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostForm extends Component {
+  render(){
+    return(
+      <div>
+        <h3>Create article</h3>
+      </div>
+    )
+  }
+}
+
+export default PostForm;
