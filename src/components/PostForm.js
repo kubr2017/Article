cea2d4045eaa56a056c606e8145d1b264e91a41a
r <kubr2017@gmail.com>
2019-05-11 23:46:58 -0400

    Feat: Add event handler to form.

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 
 class PostForm extends Component {
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const title = this.getTitle.value;
+    const message = this.getMessage.value;
+    const data = {
+      id:1,
+      title,
+      message
+    }
+  }
   render(){
     return(
       <div>
         <h3>Create article</h3>
-        <form>
-          <input required type='text' placeholder='Input title'/><br/><br/>
-          <textarea required rows='4' placeholder='Message'/><br/><br/>
+        <form onSubmit={this.handleSubmit}>
+          <input ref={input => this.getTitle = input} required type='text' placeholder='Input title'/><br/><br/>
+          <textarea ref={input => this.getMessage = input} required rows='4' placeholder='Message'/><br/><br/>
           <button>Publish</button>
         </form>
       </div>
