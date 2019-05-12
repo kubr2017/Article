43d4a8999a936186a1b34a5faebbf6342ad05958
r <kubr2017@gmail.com>
2019-05-11 22:34:58 -0400

    Feat: Fill out <AllPost>

--- src/components/AllPost.js
+++ src/components/AllPost.js
@@ -4,7 +4,11 @@ class AllPost extends Component{
   render(){
     return(
       <div>
-        <h3>AllPost</h3>
+        <h3>All Posts</h3>
+        <ul>
+          <li>
+          </li>
+        </ul>
       </div>
     )
   }
